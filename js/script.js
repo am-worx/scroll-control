7b8decf3702d90834d49bbf004a0169b54904565
Andrii Motsyk <motsyk.a@gmail.com>
2015-06-22 00:15:08 +0300

    light js scroll done

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function animateOpacity(frame, element) {
 	}
 }
 
-function slide4() {
+function frame4() {
 	var start = frameHeight * 3;
 	if ((Ypos - start) >= 0) {
 		free.style.left = ((Ypos - start) / frameHeight) > 1 ? 0 
@@ -22,7 +22,7 @@ function slide4() {
 	}
 }
 
-function slide5() {
+function frame5() {
 	var start = frameHeight * 4;
 	if ((Ypos - start) >= 0) {
 		arrow.style.width = (Ypos - start) * (308 / frameHeight) + 'px';
@@ -32,22 +32,23 @@ function slide5() {
 }
 
 window.onload = function() {
-	(window.onresize = function() {
-		frameHeight = windowHeight();
-		document.body.style.height = frameHeight * (5 + 1) + 'px';
-	})()
 	blurred = document.getElementsByClassName('blurred')[0];
 	header = document.getElementById('header');
 	form = document.getElementsByClassName('form')[0];
 	free = document.getElementsByClassName('free')[0];
 	arrow = document.getElementsByClassName('arrow')[0];
+
+	(window.onresize = function() {
+		frameHeight = windowHeight();
+		document.body.style.height = frameHeight * (5 + 1) + 'px'; // 5 is frames qty
+	})()
+
 	window.onscroll = function() {
 		Ypos = window.pageYOffset || document.documentElement.scrollTop;
-
 		animateOpacity(1, blurred);
 		animateOpacity(2, header);
 		animateOpacity(3, form);
-		slide4();
-		slide5();
+		frame4();
+		frame5();
 	}
 }
